17451e502d9a4c0d118b2ee03835a7152db70e0a
Prasan Dsouza <108275626+prasandsouza@users.noreply.github.com>
2026-06-18 20:06:08 +0530

    updated with level 2 if conditions

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -1,6 +1,7 @@
 package org;
 
 import org.DSABasics.ConditionalThinking.levelOne.SimpleIf;
+import org.DSABasics.ConditionalThinking.levelTwo.NestedIfAndMultipleCondition;
 
 
 public class Main {
@@ -8,5 +9,15 @@ public class Main {
         SimpleIf simpleIf = new SimpleIf();
 
         simpleIf.checkingSimpleIf();
+
+
+        NestedIfAndMultipleCondition nestedIfAndMultipleCondition  = new NestedIfAndMultipleCondition();
+        nestedIfAndMultipleCondition.checkForValidTriangle();
+        nestedIfAndMultipleCondition.checkTypeOftriangle();
+        nestedIfAndMultipleCondition.findingGrades(65);
+        nestedIfAndMultipleCondition.findingMultiple(100,13);
+        nestedIfAndMultipleCondition.findingMultiple(10,30);
+        nestedIfAndMultipleCondition.dayWishes(12);
+        nestedIfAndMultipleCondition.dayWishes(-65);
     }
 }
