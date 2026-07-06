d45ca11f4a2f67df64797ca5d637fe57e0a8f221
Prasan Dsouza <108275626+prasandsouza@users.noreply.github.com>
2026-07-06 18:57:47 +0530

    Adding loops and logic

--- src/main/java/org/DSABasics/LoopingAndPattern/levelFive/LogicalLoopingCombination.java
+++ src/main/java/org/DSABasics/LoopingAndPattern/levelFive/LogicalLoopingCombination.java
@@ -43,5 +43,75 @@ public class LogicalLoopingCombination {
 }
 
         //print all the number whose digit add up to a multiple of 3
+        for(int i=4;i<=100;i++){
+            int temp = i;
+            int sum = 0;
+            int lastDigit = 0;
+            while(temp!=0){
+                lastDigit = temp % 10 ;
+                sum = sum + lastDigit;
+                temp = temp /10;
+            }
+            if(sum%3==0){
+                System.out.println("Number whose digits add up to a multiple of 3: " + i);
+            }
+        }
+
+
+        // find the smallest and largest digit of given number
+        int number = 123456789;
+         int smallest = 9;
+         int largest = 0;
+         int digit;
+        while(number!=0){
+            digit = number % 10;
+            if(number % 10 >largest){
+                largest = digit;
+            }
+            if(number % 10 < smallest){
+                smallest = digit;
+            }
+            number /=10;
+        }
+        System.out.println("Smallest digit: " + smallest);
+        System.out.println("Largest digit: " + largest);
+
+        //print all numbers from 1-n whose binary representation has an even number of 1s
+
+        for(int i=0;i<100;i++){
+
+            int temp = i;
+            int temp2 = 0;
+            int count = 0;
+            int binary ;
+            int lastDigit;
+            while(temp!=0){
+                lastDigit = temp%10;
+                temp2 = lastDigit;
+                while (temp2!=0){
+                    binary = temp2%2;
+                    if(binary ==1){
+                        count++;
+                    }
+                    temp2 = temp2-binary;
+                    temp2/=2;
+                }
+                temp/=10;
+            }
+
+            if(count%2==0){
+                System.out.println("Number whose binary representation has an even number of 1s: "+i);
+            }
+
+
+            /*alternative method
+            for(int i=0;i<100;i++){
+                int count = Integer.bitCount(i);
+                if(count%2==0){
+                    System.out.println("Number whose binary representation has an even number of 1s: "+i);
+                }
+            } */
+        }
+
     }
 }
