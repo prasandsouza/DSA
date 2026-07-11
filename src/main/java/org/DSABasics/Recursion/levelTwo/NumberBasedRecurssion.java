29614b567f015ffd151e742dba69a4931f31d4f3
Prasan Dsouza <108275626+prasandsouza@users.noreply.github.com>
2026-07-11 22:19:20 +0530

    Create NumberBasedRecurssion.java

---
+++ src/main/java/org/DSABasics/Recursion/levelTwo/NumberBasedRecurssion.java
@@ -0,0 +1,27 @@
+package org.DSABasics.Recursion.levelTwo;
+
+import java.util.Map;
+
+public class NumberBasedRecurssion {
+
+    static int countTheNumberInDigit(int number){
+       if(number==0){
+           return 0;
+       }
+       return 1 +countTheNumberInDigit(number/10);
+    }
+
+    static int reverseTheNumberUsingRecurrsion(int number){
+        if(number<10){
+            return number;
+        }
+        int digits = (int) Math.log10(number);
+        int digit = number %10;
+        return (int) (digit * Math.pow(10,digits) + reverseTheNumberUsingRecurrsion(number/10));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countTheNumberInDigit(12345));
+        System.out.println(reverseTheNumberUsingRecurrsion(12345));
+    }
+}
