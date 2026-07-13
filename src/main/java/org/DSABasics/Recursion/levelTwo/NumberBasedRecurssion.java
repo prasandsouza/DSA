e01f4ed9fcb472dbace4a60a3a37a1244520516f
Prasan Dsouza <108275626+prasandsouza@users.noreply.github.com>
2026-07-13 22:25:15 +0530

    adding recurrsion number

--- src/main/java/org/DSABasics/Recursion/levelTwo/NumberBasedRecurssion.java
+++ src/main/java/org/DSABasics/Recursion/levelTwo/NumberBasedRecurssion.java
@@ -14,14 +14,34 @@ public class NumberBasedRecurssion {
     static int reverseTheNumberUsingRecurrsion(int number){
         if(number<10){
             return number;
-        }  
+        }
         int digits = (int) Math.log10(number);
         int digit = number %10;
         return (int) (digit * Math.pow(10,digits) + reverseTheNumberUsingRecurrsion(number/10));
     }
 
+    static boolean numberIsPalindromeOrNotUsingRecurrsion(int number){
+       if(number<10){
+           return true;
+       }
+       return number == reverseTheNumberUsingRecurrsion(number);
+
+    }
+
+    static int reversed = 0;
+
+    static boolean numberIsPalindromeOrNot(int number,int originalNUmber){
+        if(number==0){
+            return originalNUmber == reversed;
+        }
+        reversed = reversed*10 + number%10;
+        return numberIsPalindromeOrNot(number/10,originalNUmber);
+    }
+
     public static void main(String[] args) {
         System.out.println(countTheNumberInDigit(12345));
         System.out.println(reverseTheNumberUsingRecurrsion(12345));
+        System.out.println(numberIsPalindromeOrNotUsingRecurrsion(12322) ? "Number is Palindrome":"Number is not Palindrome");
+        System.out.println(numberIsPalindromeOrNot(12321,12321) ? "Number is Palindrome":"Number is not Palindrome");
     }
 }
---
+++ src/main/java/org/DesignPattern/DesignPattern.java
@@ -0,0 +1,4 @@
+package org.DesignPattern;
+
+public class DesignPattern {
+}
